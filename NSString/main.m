9a3551fb5df66be07bd6089d40b3c7c2961787f1
steve thompson <steven.thompson.ca@gmail.com>
2018-04-09 15:40:24 -0400

    Initial Commit

---
+++ NSString/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NSString
+//
+//  Created by steve on 2018-04-09.
+//  Copyright © 2018 steve. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
